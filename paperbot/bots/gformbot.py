81f0f363ee335e45f1d9edc0a8aef61262694eba
Jing Yang <jingyang.carl.work@gmail.com>
2024-04-05 12:45:00 -0700

    update gformbot

--- paperbot/bots/gformbot.py
+++ paperbot/bots/gformbot.py
@@ -66,8 +66,6 @@ class GFormBotICML(GFormBot):
         
     def get_paperlist(self, mode=None, as_init=False):
         paperlist = []
-        ratings = []
-        confidences = []
         for index, row in self.df.iterrows():
             id = index
             title = ''
@@ -105,8 +103,6 @@ class GFormBotICML(GFormBot):
                     rating = self.auto_split(row['Initial Ratings'])
                     confidence = self.auto_split(row['Initial Confidence'])
 
-            ratings.append(rating)
-            confidences.append(confidence)
             # list to numpy
             list2np = lambda x: np.array(list(filter(None, x))).astype(np.float64)
             rating = list2np(rating)
@@ -186,8 +182,6 @@ class GFormBotACL(GFormBot):
     def get_paperlist(self, mode=None, as_init=False):
         
         paperlist = []
-        ratings = []
-        confidences = []
         
         for index, row in self.df.iterrows():
             id = index
@@ -227,8 +221,6 @@ class GFormBotACL(GFormBot):
                     confidence = self.auto_split(row['Initial Confidence'])
                     correctness = self.auto_split(row['Initial Soundness'])
 
-            ratings.append(rating)
-            confidences.append(confidence)
             # list to numpy
             list2np = lambda x: np.array(list(filter(None, x))).astype(np.float64)
             rating = list2np(rating)
@@ -272,6 +264,135 @@ class GFormBotACL(GFormBot):
             })
         return paperlist
                 
+    def launch(self, fetch_site=False):
+        super().launch(fetch_site)
+        
+        for track in self._tracks:
+            
+            if self._year == 2024:
+                # update paperlist
+                self.summarizer.paperlist = self.get_paperlist()
+                self.summarizer.get_histogram(self._args['tname'][track], track)
+
+                # hack to copy total data to active for now
+                hist = self.summarizer.tier_hist[1]
+                hist_sum = self.summarizer.tier_hist_sum[1]
+            
+                # update paperlist
+                self.summarizer.paperlist = self.get_paperlist(mode='Rebuttal')
+                self.summarizer.paperlist_init = self.get_paperlist(mode='Rebuttal', as_init=True)
+                self.summarizer.get_histogram(self._args['tname'][track], track)
+                self.summarizer.get_transfer_matrix(self._args['tname'][track], track)
+            
+                self._summary_all_tracks[track] = self.summarizer.summarize()
+                self._summary_all_tracks[track]['thist'][0] = hist
+                self._summary_all_tracks[track]['thsum'][0] = hist_sum
+                
+class GFormBotKDD(GFormBot):
+    
+    def __init__(self, conf='', year=None, root_dir=''):
+        super().__init__(conf, year, root_dir)
+        
+        self._gform_key = 'kdd'
+        
+        self._tracks = [
+            'KDD 2024 Research Track',
+            'KDD 2024 Applied Data Science Track',
+        ]
+        
+    def get_paperlist(self, mode=None, as_init=False):
+        
+        paperlist = []
+        
+        for index, row in self.df.iterrows():
+            id = index
+            title = ''
+            keywords = ''
+            status = ''
+            
+            if 'ryr' in self._gform_key:
+                match = re.search('[a-zA-Z]', row['Rate Your Reviewer: Ratings'])
+                if match: continue
+                if row['Submitting this form for the first time? (for redundancy removal)'] == 'No': continue
+                
+                rating = self.auto_split(row['Rate Your Reviewer: Ratings'])
+                confidence = self.auto_split(row['Rate Your Reviewer: Confidences'])
+                
+            else:
+                # remove invalide response
+                match = re.search('[a-zA-Z]', row['Initial Novelty'])
+                if match: continue
+                
+                if mode == 'Rebuttal':
+                    # remove nan data
+                    if pd.isna(row['[Optional] Novelty after Rebuttal']) or not row['[Optional] Novelty after Rebuttal']:
+                        continue
+                    
+                    if as_init:
+                        novelty = self.auto_split(row['Initial Novelty'])
+                        tech_quality = self.auto_split(row['Initial Technical Quality'])
+                        confidence = self.auto_split(row['Initial Confidence'])
+                    else:
+                        novelty = self.auto_split(row['[Optional] Novelty after Rebuttal'])
+                        tech_quality = self.auto_split(row['[Optional] Technical Quality after Rebuttal'])
+                        confidence = self.auto_split(row['[Optional] Confidence after Rebuttal'])
+                    track = row['Track'].strip()
+                else:
+                    # remove redundant data
+                    if row['Submitting this form for the first time? (for redundancy removal)'] == 'No': continue
+                    
+                    novelty = self.auto_split(row['Initial Novelty'])
+                    tech_quality = self.auto_split(row['Initial Technical Quality'])
+                    confidence = self.auto_split(row['Initial Confidence'])
+                    track = row['Track'].strip()
+                    
+            # list to numpy
+            list2np = lambda x: np.array(list(filter(None, x))).astype(np.float64)
+            novelty = list2np(novelty)
+            tech_quality = list2np(tech_quality)
+            rating = 0.5 * novelty + 0.5 * tech_quality
+            confidence = list2np(confidence)
+            
+            np2avg = lambda x: 0 if not any(x) else x.mean()
+            np2coef = lambda x, y: 0 if (not any(x) or not any(y)) else np.nan_to_num(np.corrcoef(np.stack((x, y)))[0,1])
+            np2str = lambda x: ';'.join([str(y) for y in x])
+            
+            if len(rating) != len(confidence):
+                raise ValueError(f"Rating and confidence length mismatch: {len(rating)} vs {len(confidence)}; {rating} vs {confidence}")
+            
+            if np2avg(rating) > 6:
+                raise ValueError(f"Rating > 6: {np2avg(rating)}")
+            
+            extra = {
+                'rating': {
+                    'str': np2str(rating),
+                    'avg': np2avg(rating)
+                },
+                'confidence': {
+                    'str': np2str(confidence),
+                    'avg': np2avg(confidence)
+                },
+                'corr_rating_confidence': np2coef(rating, confidence),
+            }
+            
+            paperlist.append({
+                'id': id,
+                'title': title,
+                'track': track,
+                'status': status,
+                'keywords': keywords,
+                'author': '',
+                
+                'rating': extra['rating']['str'],
+                'confidence': extra['confidence']['str'],
+                
+                'rating_avg': extra['rating']['avg'],
+                'confidence_avg': extra['confidence']['avg'],
+                
+                'corr_rating_confidence': extra['corr_rating_confidence'],
+            })
+        return paperlist
+        
     def launch(self, fetch_site=False):
         super().launch(fetch_site)
         
